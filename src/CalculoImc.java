7a35ab8c61e389ab7b65e5795cd02d7def5bef1e
GABRIEL RENATO NASCIMENTO MANZOLLA <25203680@CFP127ALUNO.LOCAL>
2025-09-29 14:20:54 -0300

    pokemon

--- src/CalculoImc.java
+++ src/CalculoImc.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class CalculoImc {
 
-    int peso;
-    int altura;
-    int imc;
+    double peso;
+    double altura;
+    double imc;
     String nome;
-    double imc2;
+    String resultadoDoImc;
 
 
     public void obterDados(){
@@ -16,40 +16,51 @@ public class CalculoImc {
         nome = leitor.nextLine();
 
         System.out.println("Qual a altura dele(a)?");
-        peso = leitor.nextInt();
+        altura = leitor.nextDouble();
 
         System.out.println("e o peso?");
-        altura = leitor.nextInt();
+        peso = leitor.nextDouble();
 
         calcularImc();
     }
 
 
     public void calcularImc(){
-       double alturaEmMetros = altura / 100.0;
-        imc = peso / (alturaEmMetros * alturaEmMetros);
+        double altura2x;
+                altura2x = altura * altura;
+        imc = peso / altura2x;
 
-
+        determinarIMC();
     }
 
-    public void determinarIMC(){
-        if(imc < 18.5){
-            imc2 = "abaixo do peso"
-        } else if(){
-
-        } else if(){
-
-        } else if(){
-
-        } else if(){
-
-        } else if(){
-
-        } else if(){
+    public void determinarIMC() {
+        if (imc <= 18.5) {
+            resultadoDoImc = "abaixo do peso";
+        } else if (imc >= 18.6 && imc <= 24.9) {
+            resultadoDoImc = "como peso ideal";
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            resultadoDoImc = "levemente acima do peso";
+        } else if (imc >= 30.0 && imc <= 34.9) {
+            resultadoDoImc = "com obesidade grau 1";
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            resultadoDoImc = "com obesidade grau 2 (severa)";
+        } else if (imc >= 40.0) {
+            resultadoDoImc = "com obesidade grau 3 (mórbida)";
 
         }
+        resultados();
+    }
 
 
+    public void resultados(){
+
+        System.out.println("--------------");
+        System.out.println("o aluno " +nome+ " ");
+        System.out.println("que tem " +altura+ " metros de altura");
+        System.out.println("e " +peso+ " quilos");
+        System.out.println("--------------");
+        System.out.println("está com o imc de " +imc+ " ");
+        System.out.println("e está " +resultadoDoImc+ " ");
     }
 
 
